3cb39af36b5fde5cc60aaa97d2ca6e85b78e846e
Vickouma77 <Vickprestone20@gmail.com>
2026-03-19 19:15:12 +0300

    feat: add initial API structure with healthcheck endpoint and configuration

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # flick-api
+
+.
+├── bin
+├── cmd
+│
+└── api
+│   └── main.go
+|
+├── internal
+|
+├── migrations
+|
+├── remote
+|
+├── go.mod
+└── Makefile
+
+**bin**: Contains compiled application binaries for deployment purposes.
+**cmd/api**: Contains application specific code, i.e running server, reading and writing HTTP requests etc
+**internal**: Contains packages used by the API that are not intended to be used by external applications.
+**migrations**: SQL migration files for the database.
+**remote**: Contains configuration files and scripts for production servers
+**Makefile**: Contains files for automating common administrative tasks i.e auditing Go code, running tests, building the application, etc.
---
+++ cmd/api/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// TODO: Refactor later
+const version = "1.0.0"
+
+// Configuration settings
+type config struct {
+	port int
+	env  string
+}
+
+// Dependencies for the HTTP handlers, helpers and middlewares.
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var cfg config
+
+	// Initialize flags
+	flag.IntVar(&cfg.port, "port", 8000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
+
+	// Initialize a structure logger to write to standard out stream
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	// An instance of the application struct
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// mux: the HTTP request router for application endpoints.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// A custom HTTP server
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// Start HTTP server
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
