7b6ef7a60588fd6d07ad0959cb8a3d2ee4b18f5d
Vickouma77 <Vickprestone20@gmail.com>
2026-03-20 23:00:57 +0300

    feat: update healthcheck endpoint to return JSON response and modify production run command

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,10 @@ import (
 )
 
 func (a *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", a.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, a.config.env, version)
+
+	w.Header().Set("Content-type", "application/json")
+
+	w.Write([]byte(js))
 }
