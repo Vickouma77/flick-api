015834c64e9f51816c57961575e753acf8f129a5
Vickouma77 <Vickprestone20@gmail.com>
2026-03-26 13:52:40 +0300

    feat: implement MovieModel with CRUD methods and database connection

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"flick.io/internal/validator"
@@ -15,6 +16,9 @@ type Movie struct {
 	Genres    []string  `json:"genre,omitempty"`
 	Version   int32     `json:"version"`
 }
+type MovieModel struct {
+	DB *sql.DB
+}
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(movie.Title != "", "title", "must be provided")
@@ -28,4 +32,20 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+func (m *MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m *MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m *MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m *MovieModel) Delete(id int64) error {
+	return nil
 }
