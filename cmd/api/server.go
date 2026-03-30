6886c8039e8a0823fa3464b03e14b8a08564a47d
Vickouma77 <Vickprestone20@gmail.com>
2026-03-30 14:20:58 +0300

    feat: implement graceful server shutdown and error handling

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -17,7 +22,42 @@ func (a *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(a.logger.Handler(), slog.LevelError),
 	}
 
+	shutdownError := make(chan error)
+
+	// Starting a background goroutine
+	go func() {
+		// Quit channel that carries os.Signal values
+		quit := make(chan os.Signal, 1)
+
+		// Listen for incoming SIGINT and SIGTERM and relay to quit channel
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until a signal is
+		// received.
+		s := <-quit
+
+		a.logger.Info("shutting down server", "signal", s.String())
+
+		// Context with 30 second timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	a.logger.Info("starting server", "addr", srv.Addr, "env", a.config.env)
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	a.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
 }
