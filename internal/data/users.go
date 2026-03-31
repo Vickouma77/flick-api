728b9409069500578e2092c863f6380cdad8cdbd
Vickouma77 <Vickprestone20@gmail.com>
2026-03-31 09:56:03 +0300

    feat: add user model with password handling and bcrypt integration

---
+++ internal/data/users.go
@@ -0,0 +1,58 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User represents an individual user in the system.
+type User struct {
+	ID        int       `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+// password is a custom type containing the plaintext and hashed versions of a password for a user.
+// The plaintext field is a *pointer* to a string, so that we're able to distinguish between a
+// plaintext password not being present in the struct at all, versus a plaintext password which
+// is the empty string "".
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+// Set calculates the bcrypt hash of a plaintext password, and stores both
+// the hash and the plaintext versions in the struct.
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+// Matches checks whether the provided plaintext password matches the
+// hashed password stored in the struct, returning true if it matches
+// and false otherwise.
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, nil
+		}
+	}
+	return true, nil
+}
