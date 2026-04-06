2959ce73c3523b8b7b658796f7449579e8b79b27
Vickouma77 <Vickprestone20@gmail.com>
2026-04-06 12:07:06 +0300

    feat: add PermissionModel and permissions handling to manage user permissions

--- internal/data/models.go
+++ internal/data/models.go
@@ -11,15 +11,17 @@ var (
 )
 
 type Models struct {
-	Movies MovieModel
-	Token  TokenModel
-	Users  UserModel
+	Movies      MovieModel
+	Permissions PermissionModel
+	Token       TokenModel
+	Users       UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Token:  TokenModel{DB: db},
-		Users:  UserModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db},
+		Token:       TokenModel{DB: db},
+		Users:       UserModel{DB: db},
 	}
 }
