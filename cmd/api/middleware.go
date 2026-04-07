62c8189596f921b1dedadadf0cbea0f809377c0e
Vickouma77 <Vickprestone20@gmail.com>
2026-04-07 11:48:47 +0300

    feat: add CORS middleware and example server for handling cross-origin requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -190,3 +190,11 @@ func (a *application) requirePermission(code string, next http.HandlerFunc) http
 
 	return a.requireActivatedUser(fn)
 }
+
+func (a *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
