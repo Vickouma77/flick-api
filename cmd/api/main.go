825845c805a4aed589f88b033f0e9935e1ace5a3
Vickouma77 <Vickprestone20@gmail.com>
2026-04-09 11:25:19 +0300

    feat: add version flag to display application version and exit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -84,7 +85,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	// Initialize a structure logger to write to standard out stream
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
